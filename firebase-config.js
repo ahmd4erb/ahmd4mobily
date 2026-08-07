ad756747da371b138746d6b1c4508a5243c76df7
ahmd4erb <ahmd4erb@gmail.com>
2026-08-07 23:33:00 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,18 @@
 // ==========================================
 // firebase-config.js - إعدادات سحابة Firebase
-// ضع بيانات مشروعك الحقيقية هنا
 // ==========================================
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyARIEklzSIOZXcx08-muM-2qtEWdXizp2M",
+  authDomain: "ahmd4mobily.firebaseapp.com",
+  projectId: "ahmd4mobily",
+  storageBucket: "ahmd4mobily.firebasestorage.app",
+  messagingSenderId: "1098972913242",
+  appId: "1:1098972913242:web:d3cdd4664818f4b2908263",
+  measurementId: "G-9Y754Y66XS"
 };
 
 const app = initializeApp(firebaseConfig);
